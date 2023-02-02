2bad7d275a649991c8c00a817931e4e40dd1058d
L-Luca <99327722+L-Luca6028@users.noreply.github.com>
2023-02-02 15:34:07 +0100

    Fertige Datei

--- Scripte/led.py
+++ Scripte/led.py
@@ -1,5 +1,25 @@
 import RPi.GPIO as GPIO
 import time
+import sys
+
+#check if there are only two arguments (name of file), (number)
+if len(sys.argv) != 2:
+    print("Please give a Number between 1 and 100")
+    exit()
+
+#print(int(sys.argv[1]))
+
+#check if number is correct
+if (int(sys.argv[1]) < 0 or  int(sys.argv[1]) > 100):
+    print("Your Number is not between 1 and 100")
+    exit()
+
+else:
+    print("True")
+
+brightness = int(sys.argv[1])
+
+
 
 MOTOR_GPIO = 20
 GPIO.setmode (GPIO.BCM)
@@ -11,14 +31,14 @@ pwmMotor = GPIO.PWM(MOTOR_GPIO, 100)
 pwmMotor.start(0)
 try:
 
-    for speed in range(0,110,10):
-        pwmMotor.ChangeDutyCycle(speed)
-        print (speed)
+    while 1:
+        pwmMotor.ChangeDutyCycle(brightness)
+        print (brightness)
         time.sleep(2)
 except KeyboardInterrupt:
-    pass
+    pwmMotor.stop()
 
-pwmMotor.stop()
+
 
 
 #GPIO.output (MOTOR_GPIO, GPIO.HIGH)
