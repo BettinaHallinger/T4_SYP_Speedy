292de7faea98b1ab0496d61ce28e2c5a9f2a81a8
BettinaHallinger <bettina.hallinger@htl-saalfelden.at>
2023-05-31 11:20:36 +0200

    WLAN-page, Displaytext

--- Huibert/svelte/src/app/pages/WlanM.svelte
+++ Huibert/svelte/src/app/pages/WlanM.svelte
@@ -1,149 +1,183 @@
-<div class="switchContainer">
-
-    <div class="container">
-
-        <div class="container1">
-            <h1>WLAN-Hotspot</h1>
-<!--            <label class="switch">-->
-<!--                <input id = "hotspot" type="checkbox" bind:checked={yes} />-->
-<!--                <span class="slider round" />-->
-<!--            </label>-->
-        </div>
-
-        <div class="container1" id="hotspotCon">
-            <h1>Hotspot</h1>
-<!--        <button class="btnSubmit" disabled={!yes} on:click={connectToWLAN()}>Verbinden</button>-->
-            <button class="btnSubmit" on:click={connectToWLAN()}>Verbinden</button>
-            <p>SSID ändern</p>
-<!--        <input class="eingabe" type="string" id="wlanSSID" disabled={!yes} on:input={scream} bind:value={SSIDbestehend}>-->
-            <input class="eingabe" type="string" id="wlanSSID" on:input={scream} bind:value={SSIDbestehend}>
-            <p>Passwort ändern</p>
-<!--        <input class="eingabe" type="string" id="wlanPasswort" disabled={!yes} on:input={scream} bind:value={passwort}>-->
-            <input class="eingabe" type="string" id="wlanPasswort" on:input={scream} bind:value={passwort}>
-            <button class="btnSubmit" on:click={connectToWLAN}>Ändern</button>
-        </div>
-
-        <div class="container1" id="wlanCon">
-            <h1>Eigene SSID</h1>
-            <input class="eingabe" type="string" id ="inputBtn" placeholder="Drehteller1" on:input={scream} bind:value={ssid}>
-        </div>
-    </div>
-</div>
-
 <script>
-    let ssid = "";
-    let yes = false;
-    let SSIDbestehend = "";
-    let passwort = "";
+  let ssid = "";
+  let yes = false;
+  let SSIDbestehend = "Drehteller1";
+  let newWLANpasswort = "";
+  let newWLAN = "";
+  let passwortBestehend = "";
 
-    function scream() {
-        console.log(ssid);
-        console.log(SSIDbestehend);
-        console.log(passwort);
-    }
+  function scream() {
+    console.log(ssid);
+    console.log(SSIDbestehend);
+    console.log(passwort);
+  }
 
-    function connectToWLAN(){
+  function connectToWLAN() {}
 
-    }
+  function activateHotspot() {}
+
+  function connectToNewWLAN() {}
+
+  function changeHotspot() {}
 </script>
 
+<div class="switchContainer">
+  <div class="container">
+    <div class="container1" id="hotspotCon">
+      <div class="buttons">
+        <button class="btnSubmit" on:click={connectToWLAN}
+          >Mit WLAN verbinden</button
+        >
+        <button class="btnSubmit" on:click={activateHotspot}
+          >Hotspot aktivieren</button
+        >
+      </div>
+
+      <h1>Neues WLAN hinzufügen</h1>
+      <p>SSID</p>
+      <input
+        class="eingabe"
+        type="string"
+        id="wlanSSID"
+        on:input={scream}
+        bind:value={newWLAN}
+      />
+      <p>Passwort</p>
+      <input
+        class="eingabe"
+        type="string"
+        id="wlanPasswort"
+        on:input={scream}
+        bind:value={newWLANpasswort}
+      />
+      <button class="btnSubmit" on:click={connectToNewWLAN()}>Verbinden</button>
+
+      <h1>Eigenen Hotspot ändern</h1>
+      <!--        <button class="btnSubmit" disabled={!yes} on:click={connectToWLAN()}>Verbinden</button>-->
+      <p>SSID</p>
+      <!--        <input class="eingabe" type="string" id="wlanSSID" disabled={!yes} on:input={scream} bind:value={SSIDbestehend}>-->
+      <input
+        class="eingabe"
+        type="string"
+        id="wlanSSID"
+        on:input={scream}
+        bind:value={SSIDbestehend}
+      />
+      <p>Passwort</p>
+      <!--        <input class="eingabe" type="string" id="wlanPasswort" disabled={!yes} on:input={scream} bind:value={passwort}>-->
+      <input
+        class="eingabe"
+        type="string"
+        id="wlanPasswort"
+        on:input={scream}
+        bind:value={passwortBestehend}
+      />
+      <button class="btnSubmit" on:click={changeHotspot}>Ändern</button>
+    </div>
+  </div>
+</div>
+
 <style>
-    #connect {
+  .buttons {
+    display: flex;
+    flex-direction: row;
+  }
 
-        width: 70%;
-    }
+  #connect {
+    width: 70%;
+  }
 
-    #connect:hover {
-        background-color: #1f1f1f;
-    }
+  #connect:hover {
+    background-color: #1f1f1f;
+  }
 
-    #connect:disabled{
-        background-color: #6b6b6b;
-        color: #c1c1c1;
-    }
+  #connect:disabled {
+    background-color: #6b6b6b;
+    color: #c1c1c1;
+  }
 
-    #connect:focus{
-        outline: none;
-        background-color: #39b3ff;
-    }
+  #connect:focus {
+    outline: none;
+    background-color: #39b3ff;
+  }
 
-    .switchContainer{
-        display: flex;
-        justify-content: center;
-        flex-direction: column;
-        align-items: center;
-    }
+  .switchContainer {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+  }
 
-    .container1{
-        display: flex;
-        justify-content: center;
-        flex-direction: column;
-        align-items: center;
-        padding-bottom: 3em;
-    }
+  .container1 {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    padding-bottom: 3em;
+  }
 
-    .container{
-        padding-bottom: 3em;
-    }
+  .container {
+    padding-bottom: 3em;
+  }
 
-    .switch{
-        margin-top: 1em;
-        position: relative;
-        display:inline-flex;
-        width: 60px;
-        height: 34px;
-    }
+  .switch {
+    margin-top: 1em;
+    position: relative;
+    display: inline-flex;
+    width: 60px;
+    height: 34px;
+  }
 
-    .switch input {
-        opacity: 0;
-        width: 0;
-        height: 0;
-    }
+  .switch input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
 
-    .slider {
-        position: absolute;
-        cursor: pointer;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background-color: rgb(219, 18, 18);
-        -webkit-transition: .4s;
-        transition: .4s;
-    }
+  .slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgb(219, 18, 18);
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+  }
 
-    .slider:before {
-        position: absolute;
-        content: "";
-        height: 26px;
-        width: 26px;
-        left: 4px;
-        bottom: 4px;
-        background-color: white;
-        -webkit-transition: .4s;
-        transition: .4s;
-    }
+  .slider:before {
+    position: absolute;
+    content: "";
+    height: 26px;
+    width: 26px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+  }
 
-    input:checked + .slider {
-        background-color: rgb(22, 167, 22);
-    }
+  input:checked + .slider {
+    background-color: rgb(22, 167, 22);
+  }
 
-    /* input:focus + .slider {
+  /* input:focus + .slider {
       box-shadow: 0 0 1px rgb(22, 167, 22);
     } */
 
-    input:checked + .slider:before {
-        -webkit-transform: translateX(26px);
-        -ms-transform: translateX(26px);
-        transform: translateX(26px);
-    }
+  input:checked + .slider:before {
+    -webkit-transform: translateX(26px);
+    -ms-transform: translateX(26px);
+    transform: translateX(26px);
+  }
 
-    /* Rounded sliders */
-    .slider.round {
-        border-radius: 34px;
-    }
+  /* Rounded sliders */
+  .slider.round {
+    border-radius: 34px;
+  }
 
-    .slider.round:before {
-        border-radius: 50%;
-    }
+  .slider.round:before {
+    border-radius: 50%;
+  }
 </style>
